da3f3ed0624b0baa71dea21f348d9201444a7700
JecayChen <157844986+JecayChen@users.noreply.github.com>
2024-04-16 22:18:02 -0500

    Test Case 8

---
+++ test_8.py
@@ -0,0 +1,36 @@
+import pytest
+import json
+import os
+import openai
+import requests
+from bs4 import BeautifulSoup
+
+from module_3.sk import my_sk #Grabbing secret key from other python file
+
+def test_correct_count():
+    #check for count accuracy
+    #sample count is 985, should equal that
+    url = "https://apnews.com/article/fani-willis-hearing-trump-case-potential-conflict-c37fcf993ac33c150f3cb3e4a7a888db"
+    url = url.strip()
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    article = ""
+    for data in soup.find_all("p"):
+        article += data.get_text() + "\n"
+
+    filename = "Data/processed/article.txt"
+    with open(filename, "w") as write_file:
+            write_file.write(article)
+    
+    openai.api_key = my_sk
+
+    response = openai.ChatCompletion.create( #API call for summmarization
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful research assistant."},
+                    {"role": "user", "content": f"Summarize this article around 50 words: {article}"},
+                ],
+            )
+    page_summary = response["choices"][0]["message"]["content"]
+    
+    assert page_summary != ""
